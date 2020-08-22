e99b70728fe44c3f3d7fbaa914cacb74ed612707
惠昭 冯 <1142974782@qq.com>
2020-08-22 09:17:14 +0800

    uodate

--- Practice/Controllers/HomeController.cs
+++ Practice/Controllers/HomeController.cs
@@ -90,6 +90,13 @@ namespace Practice.Controllers
             return View(notes);
         }
 
+        public IActionResult DeleteNote(Guid id)
+        {
+            var note = _noteServices.FindNoteById(id);
+            note.IsDeleted = true;
+            return RedirectToAction("NoteList");
+        }
+
         public IActionResult Privacy()
         {
             return View();
